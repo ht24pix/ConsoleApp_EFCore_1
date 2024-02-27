aa584f8d3010f898d49e8f3f62adbb7bd488c78b
ht24pix <huutri1912@gmail.com>
2024-02-27 12:19:00 +0700

    Add project files.

---
+++ Context/KeToanDemoContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_EFCore_1.Context
+{
+    public class KeToanDemoContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=ketoandemo01;Username=postgres;Password=tri1912");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Models/AcctCompanyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_EFCore_1.Models
+{
+    public class AcctCompanyInfo
+    {
+        public int idAcct { get; set; }
+        public string acctCustomerName { get; set; }
+        public string acctVATCode { get; set; }
+        public string acctAddress { get; set; }
+    }
+}
